92ee29ae2d0446d127e52425b64ed1d5b05bb5a1
takeshi fujita <mail.takeshi.fujita@gmail.com>
2021-10-27 10:56:18 +0900

    index table cleaned

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,15 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
+  end
+  def new
+  end
+  def create
+  end
+  def edit
+  end
+  def show
+  end
+  def destroy
   end
 end
